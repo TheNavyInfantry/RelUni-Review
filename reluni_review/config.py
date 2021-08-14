ec403295777eeb9bb67a0d97f2fcea0a2c8bfe95
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-08-14 23:06:17 +0300

    ADD mail settings

--- reluni_review/config.py
+++ reluni_review/config.py
@@ -9,3 +9,9 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{config["database"]["user"]}:{config["database"]["password"]}@{config["database"]["host"]}/{config["database"]["db"]}'
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=10)
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = config['mail_username']
+    MAIL_PASSWORD = config['mail_password']
