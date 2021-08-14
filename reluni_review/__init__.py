7773221ffba056fc9ac6e0cae20840372c36e8f1
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-08-14 23:04:54 +0300

    ADD flask_mail package

--- reluni_review/__init__.py
+++ reluni_review/__init__.py
@@ -1,13 +1,15 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 from reluni_review.config import Config
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
+mail = Mail()
 
 login_manager = LoginManager()
 login_manager.login_view = "users.login"  # This line will redirect user when user tries to access unauthorized pages
@@ -22,6 +24,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    mail.init_app(app)
 
     from reluni_review.users.routes import users
     from reluni_review.posts.routes import posts
