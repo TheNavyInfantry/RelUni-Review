72c9bf487f27887a3702eac18ec3d0aefe870df3
TheNavyInfantry <nedimcanulusoy@gmail.com>
2021-08-14 23:03:23 +0300

    ADD reset request and reset token routes for forgot password

--- reluni_review/users/routes.py
+++ reluni_review/users/routes.py
@@ -1,8 +1,9 @@
 from reluni_review import db
-from reluni_review.users.forms import RegisterForm, LoginForm
+from reluni_review.users.forms import RegisterForm, LoginForm, RequestResetForm, ResetPasswordForm
 from reluni_review.models import User, Post
 from flask import render_template, redirect, url_for, flash, request, session, Blueprint
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
+from reluni_review.users.utils import send_reset_email
 
 users = Blueprint('users', __name__)
 
@@ -19,7 +20,7 @@ def register():
         db.session.add(user_to_create)
         db.session.commit()
 
-        login_user(user_to_create)
+        login_user(user_to_create) #Automatically logs the user in
 
         flash(f"Hey {user_to_create.username}! Your account has successfully created and logged into the website!",
               category='success')
@@ -84,3 +85,46 @@ def user_review(username):
         .paginate(page=page, per_page=6)
 
     return render_template("user_review.html", reviews=reviews, user=user)
+
+
+@users.route('/reset_password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    form = RequestResetForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email_address=form.email_address.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password!', category='info')
+        return redirect(url_for('login'))
+
+    return render_template('reset_request.html', form=form)
+
+@users.route('/reset_password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    user = User.verify_reset_token(token)
+
+    if user is None:
+        flash('That is an invalid or expired token!', category='warning')
+        return redirect(url_for('reset_request'))
+
+    form = ResetPasswordForm()
+
+    if form.validate_on_submit():
+        user_reset_password = User(password=form.password.data)
+
+        db.session.commit()
+
+        # login_user(user_reset_password)
+
+        flash(f"Hey {user_reset_password.username}! Your password has successfully updated!",
+              category='success')
+
+        return redirect(url_for('users.login'))
+
+    return render_template('reset_token.html', form=form)
